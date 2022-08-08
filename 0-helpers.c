77848191e68e382c42ec784b4cd44b0728a9d7aa
Nkechidoris <nkechidoris10@gmail.com>
2022-08-08 12:26:48 -0700

    isdigit

--- 0-helpers.c
+++ 0-helpers.c
@@ -13,3 +13,18 @@ int _isalpha(int c)
 	else
 		return (0);
 }
+
+/**
+ * _isdigit - checks for a digit (0 through 9).
+ * @c: input
+ *
+ * Return: 1 or 0.
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
