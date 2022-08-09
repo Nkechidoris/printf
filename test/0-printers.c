4a711f969d925c1173c8777d56376e7631662116
dii-glori <chidinmaamanze20@gmail.com>
2022-08-09 13:58:12 -0700

    fixes

--- test/0-printers.c
+++ test/0-printers.c
@@ -18,6 +18,54 @@ int print_char(const char *format, int count, va_list args)
 	return (1);
 }
 /**
+ * print_unsigned - prints unsigned digit
+ * @format: character string
+ * @count: number of characters printed
+ * @args: argument
+ * Return: 1 if true
+ */
+int print_unsigned(const char *format, int count, va_list args)
+{
+	char c = format[count];
+	uint64_t num;
+	char buff[30];
+	int retval;
+
+	num = (va_arg(args, uint64_t));
+
+	if (c == 'u')
+	{
+		unsigned_num_to_str(num, 10, buff);
+		retval = _strlen(buff);
+		print_str(buff);
+	}
+	return (retval);
+}
+/**
+ * print_signed - prints signed digit
+ * @format: character string
+ * @count: number of characters printed
+ * @args: argument
+ * Return: 1 if true
+ */
+int print_signed(const char *format, int count, va_list args)
+{
+	char c = format[count];
+	int64_t num;
+	char buff[30];
+	int retval;
+
+	num = (va_arg(args, int64_t));
+
+	if (c == 'i' || c == 'd')
+	{
+		signed_num_to_str(num, 10, buff);
+		retval = _strlen(buff);
+		print_str(buff);
+	}
+	return (retval);
+}
+/**
  * print_string - prints a string
  * @format: character string
  * @count: number of characters printed
@@ -42,13 +90,11 @@ int print_string(const char *format, int count, va_list args)
 		print_str(s);
 		free(s);
 	}
-/*
- *	else if (c == 'R')
- *	{
- *		s = (_rot13(s));
- *		print_str(s);
- *		free(s);
- *	}
- */
+	else if (c == 'R')
+	{
+		s = (_rot13(s));
+		print_str(s);
+		free(s);
+	}
 	return (retval);
 }
